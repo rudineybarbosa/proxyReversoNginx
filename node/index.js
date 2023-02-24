648c35bf0db0173eaed423e9dc47a079ac6718da
rudiney <rudiney1h@gmail.comm>
2023-02-23 21:14:18 -0300

    configurando mysql no docker

--- node/index.js
+++ node/index.js
@@ -1,9 +1,44 @@
 const express = require('express')
 const app = express()
 const porta = 3000
+const config = {
+  host: 'db',//nome do nosso container de banco de dados definido no docker-compose
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const mysql = require('mysql')
+var connection = mysql.createConnection(config)
+
+const insert = `INSERT INTO people(name) values ('Rudiney')`
+connection.query(insert)
+connection.end()
 
 app.get('/', (req, resp) => {
-  resp.send('<h1>Teste com Express!!!</h1>')
+  const connection = mysql.createConnection(config)
+
+  connection.query('SELECT name FROM people', (err, rows, fields) => {
+    if (!err) {
+      var names = "";
+      rows.forEach(row => {
+        names = names + `<li>${row.name}</li>`          
+      })
+
+      resp.send(`
+        <h1>Full Cycle Rocks!</h1>
+        <br>
+        <ul>`
+          +          
+          names
+          +
+        `</ul>`
+      )
+    } else {
+      console.log(err);
+    }
+  });    
+  connection.end()
 })
 
 app.listen(porta, ()=> {
